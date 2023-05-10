96fc7c078133246e263831a65e9bb915769a8e30
Greg Schneider <greg53x@msn.com>
2023-05-09 23:18:09 -0600

    if statement for parameters when aqi >=401

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 // AQI = Air Quality Index
 // PM2.5 = Particulate Matter 2.5 micrometer particles
-function PM25ConcentrationFromAQI(AQI) {}
+function PM25ConcentrationFromAQI(AQI) {
+  let Ih;
+  let Il;
+  let BPh;
+  let BPl;
+
+  if (AQI >= 401) {
+    Ih = 500;
+    Il = 401;
+    BPh = 500.4;
+    BPl = 350.5;
+  }
+  console.log(BPh);
+}
+PM25ConcentrationFromAQI(450);
