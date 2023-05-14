4f1c6f4e36efa26ddc01f64d1f53380fb2975fdb
Greg Schneider <greg53x@msn.com>
2023-05-13 20:50:22 -0600

    move pm2.5 function to math module

--- src/index.js
+++ src/index.js
@@ -1,21 +1,5 @@
-// AQI = Air Quality Index
-// PM2.5 = Particulate Matter 2.5 micrometer particles
-// Ve = Minute Volume, the amount of air breathed usually Liters/minute
-function PM25ConcentrationFromAQI(AQI) {
-  let Ih;
-  let Il;
-  let BPh;
-  let BPl;
+import calculateMinuteVolumeFromHeartRate, {
+  PM25ConcentrationFromAQI,
+} from "./math";
 
-  if (AQI >= 401) {
-    Ih = 500;
-    Il = 401;
-    BPh = 500.4;
-    BPl = 350.5;
-  }
-
-  return (
-    (AQI + ((Ih - Il) / (BPh - BPl)) * BPl - Il) / ((Ih - Il) / (BPh - BPl))
-  );
-}
 console.log(PM25ConcentrationFromAQI(401));
--- src/math.js
+++ src/math.js
@@ -1,1 +1,23 @@
+// AQI = Air Quality Index
+// PM2.5 = Particulate Matter 2.5 micrometer particles
+// Ve = Minute Volume, the amount of air breathed usually Liters/minute
+
 export default function calculateMinuteVolumeFromHeartRate(heartrate) {}
+
+export function PM25ConcentrationFromAQI(AQI) {
+  let Ih;
+  let Il;
+  let BPh;
+  let BPl;
+
+  if (AQI >= 401) {
+    Ih = 500;
+    Il = 401;
+    BPh = 500.4;
+    BPl = 350.5;
+  }
+
+  return (
+    (AQI + ((Ih - Il) / (BPh - BPl)) * BPl - Il) / ((Ih - Il) / (BPh - BPl))
+  );
+}
