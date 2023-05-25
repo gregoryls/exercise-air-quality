c7d3a8a4fee7093d38aedd662cd9077bce1b40d2
Greg Schneider <greg53x@msn.com>
2023-05-24 23:12:42 -0600

    function to display percent of cigarette breathed via pm2.5

--- src/DOM.js
+++ src/DOM.js
@@ -5,4 +5,9 @@ export function insertUserPM25Breathed(mass) {
   massOutput.append(p);
 }
 
-export function insertUserCigarettePercentage(percent) {}
+export function insertUserCigarettePercentage(percent) {
+  const p = document.createElement("p");
+  const cigarettePercentage = document.getElementById("cigarettePercentage");
+  p.textContent = percent;
+  cigarettePercentage.append(p);
+}
