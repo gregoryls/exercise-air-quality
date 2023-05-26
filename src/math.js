fb831fe35632ea48b255abb58890b5b68acbe8b3
Greg Schneider <greg53x@msn.com>
2023-05-25 23:16:38 -0600

    wrap exercise inputs in a div for styling

--- src/math.js
+++ src/math.js
@@ -107,6 +107,8 @@ export function calculateUserPM25Mass(minutes) {
     PM25Concentration,
     userVolumeBreathed
   );
+
+  // round result to nearest integer
   const userPM25MassRounded = Math.round(userPM25Mass);
   return userPM25MassRounded;
 }
@@ -115,6 +117,8 @@ export function convertToCigarettePercentage(pm25) {
   // pm25 comes in as micrograms, a common cigarette has 12,000 micrograms of PM2.5
   // mulitply but 100 for percentage of a cigarette breathed for the given PM2.5 mass
   const cigarettePercent = (pm25 / 12000) * 100;
+
+  // round result to two decimal places
   const cigarettePercentRounded = Number(cigarettePercent.toFixed(2));
   return cigarettePercentRounded;
 }
