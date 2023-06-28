a35d21f4d9e602aaae61d475240ce9fd86e51ea1
Greg Schneider <greg53x@msn.com>
2023-06-28 00:07:38 -0600

    comment out standalone large mass plot pieces

--- src/index.js
+++ src/index.js
@@ -145,31 +145,27 @@ const layoutCVD = {
 
 const dataCVDCombined = [dataCVD, dataCVDLargeMass];
 
-const layoutCVDLargeMass = {
-  paper_bgcolor: "#00000000",
-  plot_bgcolor: "#c1f0c1",
-  font: { color: "black" },
-  title: "Relative Risk CVD Mortality",
+// const layoutCVDLargeMass = {
+//   paper_bgcolor: "#00000000",
+//   plot_bgcolor: "#c1f0c1",
+//   font: { color: "black" },
+//   title: "Relative Risk CVD Mortality",
 
-  domain: {
-    x: [0.7, 1],
-    y: [0, 0.3],
-  },
-  xaxis: {
-    title: "Chronic Daily Exposure of PM2.5 mass (mg)",
-    gridcolor: "black",
-    anchor: "y",
-    domain: [0, 0.3],
-  },
-  yaxis: {
-    title: "Relative Risk",
-    gridcolor: "black",
-    anchor: "x",
-    domain: [0.7, 1],
-  },
-  hovermode: "closest",
-  hoverdistance: -1,
-};
+//   domain: {
+//     x: [0.7, 1],
+//     y: [0, 0.3],
+//   },
+//   xaxis: {
+//     title: "Chronic Daily Exposure of PM2.5 mass (mg)",
+//     gridcolor: "black",
+//   },
+//   yaxis: {
+//     title: "Relative Risk",
+//     gridcolor: "black",
+//   },
+//   hovermode: "closest",
+//   hoverdistance: -1,
+// };
 
 const configGeneric = { responsive: true };
 
@@ -213,12 +209,12 @@ Plotly.newPlot(
   layoutLungCancer,
   configGeneric
 );
-Plotly.newPlot(
-  "CVDPlotLargeMass",
-  dataCVDLargeMass,
-  layoutCVDLargeMass,
-  configGeneric
-);
+// Plotly.newPlot(
+//   "CVDPlotLargeMass",
+//   dataCVDLargeMass,
+//   layoutCVDLargeMass,
+//   configGeneric
+// );
 Plotly.newPlot(
   "lungCancerPlotLargeMass",
   dataLungCancerLargeMass,
