5cb1d2e8e7ce865dcc7a2482a846653fd1e44efc
Greg Schneider <greg53x@msn.com>
2025-10-15 18:19:07 -0600

    equation of time function

--- src/airConditionPollution.js
+++ src/airConditionPollution.js
@@ -7,9 +7,17 @@ function solarDeclinationAngle(day) {
   // takes numerical day starting from Jan 1 = 1. returns angle in degrees.
   return 23.45 * Math.sin((2 * Math.PI * (284 + day)) / 365);
 }
-console.log(solarDeclinationAngle(81));
+
+function equationOfTime(day, year) {
+  // numerical day starting from Jan 1 = 1. numerical current year (or target year)
+  const D =
+    6.24004077 + ((2 * Math.PI) / 365.25) * (365.25 * (year - 2000) + day);
+  const minuteOffset = -7.659 * Math.sin(D) + 9.863 * Math.sin(2 * D + 3.5932);
+  return minuteOffset;
+}
 
 function sunHourAngle(localTime, localLongitude, standardMeridian) {
+  // slc = -111.84, standard = -105
   // solar time in decimal hours, e.g. 1:30PM = 13.5 hours
   // longitudes in degrees to calculate offset from the timezone standard
   return 15 * (localTime + (standardMeridian - localLongitude) / 15 - 12);
